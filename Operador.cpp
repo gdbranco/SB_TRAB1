8254680d0e20ac3d089f874befd1d392dda7d707
Branco <gdbranco@gmail.com>
2014-09-27 21:25:28 -0300

    Lista de diretivas validas

--- Operador.cpp
+++ Operador.cpp
@@ -1,48 +1,111 @@
-#include<algorithm>
-#include<string>
-
 #include"Operador.h"
-
-vector<string> Operador::Validos;
-void Operador::setValidos()
-{
-	fstream outarq;
-	outarq.open(operadoresV);
-	string s;
-	while(getline(outarq,s))
-	{
-		if(s.compare(""))
-			Validos.push_back(s);
-	}
-	outarq.close();
-}
-
-int Operador::pesquisar(string pesquisa)
-{
-	for(unsigned int i=0;i<Validos.size();i++)
-	{
-		if(!pesquisa.compare(Validos.at(i)))
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void Operador::showValidos()
-{
-	for(unsigned int i=0;i<Validos.size();i++)
-	{
-		cout<< Validos.at(i) <<endl;
-	}
-}
-
+vector<inst> Operador::inst_validas;
+vector<diretiva> Operador::diretivas_validas;
+/***
+Done: Lista de validacao
+Todo: Operador atual, pesquisa na lista de validacao*/
 Operador::Operador(string _op)
 {
-	setValor(_op);
+    setValor(_op);
 }
 
 void Operador::validar(string _op)
 {
     throw invalid_argument("Operador/Diretiva invalidos.");
+}
+/************************************/
+void Operador::setInst_validas()
+{
+    fstream outarq;
+    outarq.open(operadoresV);
+    string s;
+    char *pch;
+    vector<string> aux;
+    do
+    {
+        getline(outarq,s);
+    }
+    while(s!=START_INST);
+    while(getline(outarq,s))
+    {
+        if(!s.compare(END_INST))
+            break;
+        if(s.compare(""))
+        {
+            pch = strtok((char*)s.c_str(),"\t ");
+            while(pch!=NULL)
+            {
+                aux.push_back(string(pch));
+                pch = strtok(NULL,"\t ");
+            }
+            inst_validas.push_back(inst(aux.at(0),\
+                                        atoi(aux.at(2).c_str()),\
+                                        atoi(aux.at(3).c_str())));
+            aux.clear();
+        }
+        else
+            continue;
+    }
+    outarq.close();
+}
+
+void Operador::setDiretivas_validas()
+{
+    fstream outarq;
+    outarq.open(operadoresV);
+    string s;
+    char *pch;
+    vector<string> aux;
+    do
+    {
+        getline(outarq,s);
+    }
+    while(s!=START_DIR);
+    while(getline(outarq,s))
+    {
+        if(!s.compare(END_DIR))
+            break;
+        if(s.compare(""))
+        {
+            pch = strtok((char*)s.c_str(),"\t ");
+            while(pch!=NULL)
+            {
+                aux.push_back(string(pch));
+                pch = strtok(NULL,"\t ");
+            }
+            diretivas_validas.push_back(_diretiva(aux.at(0),\
+                                        atoi(aux.at(1).c_str()),\
+                                        atoi(aux.at(3).c_str())));
+            aux.clear();
+        }
+        else
+            continue;
+    }
+    outarq.close();
+}
+
+int Operador::pesquisar(string pesquisa)
+{
+    for(unsigned int i=0; i<inst_validas.size(); i++)
+    {
+        if(!pesquisa.compare(inst_validas.at(i).nome))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void Operador::showValidos()
+{
+    cout << "-----INSTRUCOES VALIDAS-----\n";
+    for(unsigned int i=0; i<inst_validas.size(); i++)
+    {
+        cout<< inst_validas.at(i) <<endl;
+    }
+    cout << "-----DIRETIVAS VALIDAS-----\n";
+    for(unsigned int i=0;i<diretivas_validas.size();i++)
+    {
+        cout << diretivas_validas.at(i) << endl;
+    }
 }
--- Operador.h
+++ Operador.h
@@ -3,16 +3,60 @@
 #include "Basico.h"
 #include <vector>
 #include <fstream>
-#define operadoresV "operadoresValidos.txt"
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#define operadoresV "inst_diretiva.txt"
+#define START_INST "START_INST"
+#define END_INST "END_INST"
+#define START_DIR "START_DIR"
+#define END_DIR "END_DIR"
+using namespace std;
+typedef struct _inst
+{
+    int tamanho;
+    string nome;
+    int codigo;
+    _inst(string _nome, int _codigo, int _tamanho)
+    {
+        tamanho = _tamanho;
+        codigo  = _codigo;
+        nome    = _nome;
+    }
+    friend ostream& operator<<(ostream& os, const _inst& it)
+    {
+        os << it.nome << ' ' << it.codigo << ' ' << it.tamanho;
+        return os;
+    }
+}inst;
+typedef struct _diretiva
+{
+    string nome;
+    int operandos;
+    int tamanho;
+    _diretiva(string _nome, int _operandos, int _tamanho)
+    {
+        tamanho    = _tamanho;
+        operandos  = _operandos;
+        nome       = _nome;
+    }
+    friend ostream& operator<<(ostream& os, const _diretiva& it)
+    {
+        os << it.nome << ' ' << it.operandos << ' ' << it.tamanho;
+        return os;
+    }
+}diretiva;
 class Operador:public TipoBasico
 {
 private:
 	void validar(string);
-	static vector<string> Validos;
+	static vector<inst> inst_validas;
+	static vector<diretiva> diretivas_validas;
+	void setInst_validas();
+	void setDiretivas_validas();
 public:
-	Operador(){setValidos();};
+	Operador(){setInst_validas();setDiretivas_validas();};
 	Operador(string);
-	void setValidos();
 	void showValidos();
 	int pesquisar(string);
 };
